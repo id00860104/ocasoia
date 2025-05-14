ac2b17af424079fdd43421acacb2a7e1cd2c5c0e
id00860104 <id00860104@usal.es>
2025-05-14 19:07:59 +0200

    Update ocasoIA.js

--- ocasoIA.js
+++ ocasoIA.js
@@ -1,39 +1,3 @@
-'use strict';
-
-const elemToggleFunc = function(elem){ elem.classList.toggle('active'); }
-
-const navbar = document.querySelector('[data-navbar]');
-const navOpenBtn = document.querySelector('[data-nav-open-btn]');
-const navCloseBtn = document.querySelector('[data-nav-close-btn]');
-const overlay = document.querySelector('[data-overlay]');
-
-const navElemArr = [navCloseBtn, navOpenBtn, overlay];
-
-for(let i = 0; i < navElemArr.length; i++ ) {
-    navElemArr[i].addEventListener('click', function () { 
-        elemToggleFunc(navbar);
-        elemToggleFunc(overlay);
-    })
-}
-
-//Sticky Header
-
-const header = document.querySelector('[data-header]');
-
-window.addEventListener('scroll', function(){
-    if(window.scrollY >= 10) { header.classList.add('active'); 
-    } else { header.classList.remove('active'); }
-})
-
-// Go-top button vanishing
-
-const goTopBtn = document.querySelector('[data-go-top]');
-window.addEventListener('scroll', function() {
-    if(window.scrollY >= 800) { goTopBtn.classList.add('active'); }
-    else { goTopBtn.classList.remove('active'); }
-})
-
-
 /* CITAS Y RECORDATORIOS */
 document.addEventListener('DOMContentLoaded', function() {
     const addPersonBtn = document.getElementById('addPersonBtn');
@@ -115,3 +79,38 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+'use strict';
+
+const elemToggleFunc = function(elem){ elem.classList.toggle('active'); }
+
+const navbar = document.querySelector('[data-navbar]');
+const navOpenBtn = document.querySelector('[data-nav-open-btn]');
+const navCloseBtn = document.querySelector('[data-nav-close-btn]');
+const overlay = document.querySelector('[data-overlay]');
+
+const navElemArr = [navCloseBtn, navOpenBtn, overlay];
+
+for(let i = 0; i < navElemArr.length; i++ ) {
+    navElemArr[i].addEventListener('click', function () { 
+        elemToggleFunc(navbar);
+        elemToggleFunc(overlay);
+    })
+}
+
+//Sticky Header
+
+const header = document.querySelector('[data-header]');
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY >= 10) { header.classList.add('active'); 
+    } else { header.classList.remove('active'); }
+})
+
+// Go-top button vanishing
+
+const goTopBtn = document.querySelector('[data-go-top]');
+window.addEventListener('scroll', function() {
+    if(window.scrollY >= 800) { goTopBtn.classList.add('active'); }
+    else { goTopBtn.classList.remove('active'); }
+})
